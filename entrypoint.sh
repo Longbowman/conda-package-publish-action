ab528a4d3f27505e14c77e9cc816acc79de37a24
Longbowman <agogorishvili@gmail.com>
2020-01-16 01:04:19 +0400

    add mdtraj installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,7 @@ check_if_meta_yaml_file_exists() {
 
 upload_package(){
     conda config --set anaconda_upload yes
+    conda install -c conda-forge mdtraj
     anaconda login --username $INPUT_ANACONDAUSERNAME --password $INPUT_ANACONDAPASSWORD
     conda build .
     anaconda logout

9effe83777e04d71e68237f4f024c932dbffbd9e
Longbowman <agogorishvili@gmail.com>
2020-02-26 11:48:46 +0400

    add init

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,7 @@ upload_package(){
     apt-get update
     apt-get install -y build-essential
     conda create -n myenv python=3.6 -c conda-forge
+    conda init bash
     conda activate myenv
     conda install --yes pip
     conda install --yes -c conda-forge numpy=1.16.5 cython=0.26.1

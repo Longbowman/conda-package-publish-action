b5b81b46bb522da45627c04a1876e4df36bd89a2
Longbowman <agogorishvili@gmail.com>
2020-03-04 14:41:54 +0400

    add init bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,7 @@ upload_package(){
     conda config --set anaconda_upload yes
     apt-get update
     apt-get install -y build-essential
+    conda init bash
     conda create -n myenv python=3.6
     conda activate myenv
     echo $PWD
